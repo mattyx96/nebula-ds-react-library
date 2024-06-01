e7c71fdf6734b13aa7529c2336fae8870373d708
Matteo Omicini <grandemattyx@gmail.com>
2024-06-01 22:08:41 +0200

    new release v0.0.9

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-export * as config from 'nebula-ds-tokens'
 export * from './src/components/button/Button';
 export * from './src/components/button/IconButton.tsx';
 export * from './src/components/icon/Icon.tsx';
